e55f7abc353739a5aaaaeab5f954434406a441a4
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-01-31 14:42:19 -0500

    Update SoulBehavior.cs

--- Assets/Scripts/SoulBehavior.cs
+++ Assets/Scripts/SoulBehavior.cs
@@ -1,32 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class SoulBehavior : MonoBehaviour
 {
+    public enum TYPE
+    {
+        RED = 0,
+        GREEN,
+        BLUE
+    }
+
+    [Serializable]
+    public struct ColorSet
+    {
+        [ColorUsage(true, true)]
+        public Color mainColor;
+        [ColorUsage(true, true)]
+        public Color emissiveColor;
+    }
+    
     private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
-    private static readonly int AlbedoColor = Shader.PropertyToID("AlbedoColor");
-    private static readonly int EmissionColor = Shader.PropertyToID("EmissionColor");
-
-
+    private static readonly int AlbedoColor = Shader.PropertyToID("Color_408732C4");
+    private static readonly int EmissionColor = Shader.PropertyToID("Color_C908B841");
     
     protected static PlayerController playerController;
     
     protected bool lighton { get; private set; }
 
+    [SerializeField]
+    protected TYPE type;
+
     //====================================================================================================================//
 
-    public int type;
     public float timeToChangeDirection;
 
-    [FormerlySerializedAs("soulCol")] 
-    public Color soulColor;
-    
     public GameObject particleEffectPrefab;
 
 
     private new Renderer renderer;
+
+    [SerializeField]
+    private ColorSet[] ColorSets;
 
 
     //====================================================================================================================//
@@ -34,17 +51,9 @@ public class SoulBehavior : MonoBehaviour
     // Start is called before the first frame update
     protected virtual void Start()
     {
-        //Find the renderer, and instance the material
-        renderer = GetComponentInChildren<Renderer>();
-        var shader = renderer.material.shader;
-        renderer.material = new Material(shader);
-
-        SetColor(soulColor);
-
         if (!playerController)
             playerController = FindObjectOfType<PlayerController>();
-
-
+        
         lighton = playerController.FlashLightOn;
     }
 
@@ -59,32 +68,43 @@ public class SoulBehavior : MonoBehaviour
     }
 
     //====================================================================================================================//
+
+    public void Init(in TYPE type)
+    {
+        //------------------------------------------------------------------------------------------------------------//
+        
+        //Find the renderer, and instance the material
+        renderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        var shader = renderer.material.shader;
+        renderer.material = new Material(shader);
+
+        //------------------------------------------------------------------------------------------------------------//
+        
+        this.type = type;
+        
+        //TODO Get the Colors
+        SetColor(ColorSets[(int)type]);
+    }
     
     public virtual void CaptureSoul()
     {
-        print("collided");
+        //print("collided");
 
         Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
-       
-       switch (type)
-       {
-           case 0:
-           playerController.typeACount ++;
-           break;
-           case 1:
-           playerController.typeBCount ++;
-           break;
-           case 2:
-           playerController.typeCcount ++;
-           break;
-       }
+
+        playerController.CollectSoul(type);
         Destroy(gameObject);
     }
 
 
-    protected void SetColor(in Color color)
+    protected void SetColor(in ColorSet colorSet)
     {
-        renderer.material.SetColor(AlbedoColor, color);
+        SetColor(colorSet.mainColor, colorSet.emissiveColor);
+    }
+    protected void SetColor(in Color mainColor, in Color emissiveColor)
+    {
+        renderer.material.SetColor(AlbedoColor, mainColor);
+        renderer.material.SetColor(EmissionColor, emissiveColor);
     }
 }
 
